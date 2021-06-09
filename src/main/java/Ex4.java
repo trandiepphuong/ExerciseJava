60a94f376a96331f879d328c49808ba4928c11fe
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-09 11:49:10 +0700

    exercise04

---
+++ src/main/java/Ex4.java
@@ -0,0 +1,17 @@
+public class Ex4 {
+    public int fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public String main(int n) {
+        String s = "";
+        for (int i = 0; i < n; i++) {
+            s+=fibonacci(i);
+        }
+        return  s;
+    }
+}
