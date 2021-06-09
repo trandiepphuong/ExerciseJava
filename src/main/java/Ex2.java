262f1d16e9a4dfb03594f4b81924b2e917e3f957
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-09 18:13:30 +0700

    update-exercise-02

--- src/main/java/Ex2.java
+++ src/main/java/Ex2.java
@@ -1,9 +1,9 @@
 public class Ex2 {
-    public static int sum(int n){
-        int s=0;
-        while(n>0){
-            s+=n%10;
-            n=n/10;
+    public int sum(int n) {
+        int s = 0;
+        while (n > 0) {
+            s += n % 10;
+            n = n / 10;
         }
         return s;
     }
--- src/test/java/Ex2Test.java
+++ src/test/java/Ex2Test.java
@@ -5,11 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Ex2Test {
 
     @Test
-    public void test(){
+    public void test() {
         Ex2 ex2 = new Ex2();
-        assertEquals(ex2.sum(1), 1);
-        assertEquals(ex2.sum(100), 1);
-        assertEquals(ex2.sum(0), 0);
-        assertEquals(ex2.sum(123456789), 45);
+        assertEquals(new Ex2().sum(1), 1);
+        assertEquals(new Ex2().sum(100), 1);
+        assertEquals(new Ex2().sum(0), 0);
+        assertEquals(new Ex2().sum(123456789), 45);
     }
 }
