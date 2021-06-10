b9b7fde027896a260a7a4ea206126882948d5e6d
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-10 08:20:14 +0700

    update-exercise-03

--- src/main/java/Ex3.java
+++ src/main/java/Ex3.java
@@ -1,19 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
 public class Ex3 {
-    public static String PTTSNT(int n){
-        String s = "";
+    public static String phantichNT(int n){
+        String result = "";
         int i = 2;
-        List<Integer> listNumbers = new ArrayList<Integer>();
-        // phân tích
         while (n > 1) {
             if (n % i == 0) {
                 n = n / i;
-                s+=i;
+                result+=i;
             } else {
                 i++;
             }
         }
-        return s;
+        return result;
     }
 }
--- src/test/java/Ex3Test.java
+++ src/test/java/Ex3Test.java
@@ -5,11 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Ex3Test {
 
     @Test
-    public void test(){
+    public void test_phantichNT(){
         Ex3 ex3 = new Ex3();
-        assertEquals(ex3.PTTSNT(1), "");
-        assertEquals(ex3.PTTSNT(600), "222355");
-        assertEquals(ex3.PTTSNT(6), "23");
-
+        assertEquals(ex3.phantichNT(1), "");
+        assertEquals(ex3.phantichNT(600), "222355");
+        assertEquals(ex3.phantichNT(6), "23");
     }
 }
