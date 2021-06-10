e7d06603d6cc52f60a3b3540682ee0d74b8aea50
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-10 09:15:38 +0700

    add-exercise-07

---
+++ src/main/java/Ex7.java
@@ -0,0 +1,9 @@
+public class Ex7 {
+    public int giaithua(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
