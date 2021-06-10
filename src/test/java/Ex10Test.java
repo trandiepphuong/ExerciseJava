8c2143c7e0cb049f0f3b0dcdeadd388dc6391b7e
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-10 10:21:46 +0700

    add-exercise-10

--- src/test/java/Ex10Test.java
+++ src/test/java/Ex10Test.java
@@ -5,8 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Ex10Test {
 
     @Test
-    public void test_thu(){
-        assertEquals(new Ex10().thu(10,6,2021), "thu nam");
-        assertEquals(new Ex10().thu(11,6,2021), "thu sau");
+    public void test_thu() {
+        assertEquals(new Ex10().thu(10, 6, 2021), "thu nam");
+        assertEquals(new Ex10().thu(11, 6, 2021), "thu sau");
     }
 }
