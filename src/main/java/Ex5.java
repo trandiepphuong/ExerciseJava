36221476c0c7e5d93b4ce19fcfed6e9ea732c15b
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-10 08:35:07 +0700

    update-exercise-05

--- src/main/java/Ex5.java
+++ src/main/java/Ex5.java
@@ -1,31 +1,29 @@
 public class Ex5 {
     public String giaiPTBac2(float a, float b, float c) {
-        String s = "";
-        // kiểm tra các hệ số
+        String result = "";
         if (a == 0) {
             if (b == 0) {
-                s += ("VN");
+                return "VN";
             } else {
-                s +="x = "+ (-c / b);
+                return "x = " + (-c / b);
             }
         }
-        if (a!=0) {
+        if (a != 0) {
             float delta = b * b - 4 * a * c;
-            float x1;
-            float x2;
-            // tính nghiệm
             if (delta > 0) {
+                float x1;
+                float x2;
                 x1 = (float) ((-b + Math.sqrt(delta)) / (2 * a));
                 x2 = (float) ((-b - Math.sqrt(delta)) / (2 * a));
-                s += ("x1 = " + x1 + ", x2 = " + x2);
+                result += ("x1 = " + x1 + ", x2 = " + x2);
             } else if (delta == 0) {
-                x1 = (-b / (2 * a));
-                s += ("x1 = x2 = " + x1);
+                float x;
+                x = -b / (2 * a);
+                result += "x1 = x2 = " + x;
             } else {
-                s += ("VN");
+                result += "VN";
             }
         }
-        return s;
+        return result;
     }
-
 }
--- src/test/java/Ex5Test.java
+++ src/test/java/Ex5Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Ex5Test {
 
     @Test
-    public void test(){
+    public void test_giaiPTBac2(){
         assertEquals(new Ex5().giaiPTBac2(1,-3,2), "x1 = 2.0, x2 = 1.0");
         assertEquals(new Ex5().giaiPTBac2(1,-4,4), "x1 = x2 = 2.0");
         assertEquals(new Ex5().giaiPTBac2(1,1,2), "VN");
