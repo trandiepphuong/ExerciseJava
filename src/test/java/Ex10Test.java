4b519b6993f79f9d2cd55cb7aa47c7b4af9c89dd
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-10 10:25:28 +0700

    add-exercise-10

--- src/test/java/Ex10Test.java
+++ src/test/java/Ex10Test.java
@@ -6,7 +6,12 @@ public class Ex10Test {
 
     @Test
     public void test_thu() {
+        assertEquals(new Ex10().thu(1, 2, 2021), "thu hai");
+        assertEquals(new Ex10().thu(6, 6, 2021), "chu nhat");
+        assertEquals(new Ex10().thu(8, 6, 2021), "thu ba");
+        assertEquals(new Ex10().thu(9, 6, 2021), "thu tu");
         assertEquals(new Ex10().thu(10, 6, 2021), "thu nam");
         assertEquals(new Ex10().thu(11, 6, 2021), "thu sau");
+        assertEquals(new Ex10().thu(12, 6, 2021), "thu bay");
     }
 }
