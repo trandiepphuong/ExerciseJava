d24e8d9fe750b28b21bc851b1f6dc64a0c24dcbc
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-10 11:07:03 +0700

    add-exercise-11

--- src/main/java/Ex12.java
+++ src/main/java/Ex12.java
@@ -4,10 +4,11 @@ public class Ex12 {
     public static double canbac2(int a) {
         double fn = a;
         while (Math.abs(fn * fn - a) >= PRECISE)
-            fn = a/(2*fn)+fn/2;
+            fn = a / (2 * fn) + fn / 2;
         return fn;
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         System.out.println(canbac2(5));
     }
 }
