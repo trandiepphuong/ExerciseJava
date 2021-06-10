f7e4d8b223cfa2639e55dd4d225337e3c18a294c
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-10 11:08:14 +0700

    exercise-12

--- src/main/java/Ex12.java
+++ src/main/java/Ex12.java
@@ -1,14 +1,11 @@
 public class Ex12 {
-    static double PRECISE = 0.0001;
+    double PRECISE = 0.0001;
 
-    public static double canbac2(int a) {
+    public double canbac2(int a) {
         double fn = a;
         while (Math.abs(fn * fn - a) >= PRECISE)
             fn = a / (2 * fn) + fn / 2;
         return fn;
     }
 
-    public static void main(String args[]) {
-        System.out.println(canbac2(5));
-    }
 }
--- src/test/java/Ex12Test.java
+++ src/test/java/Ex12Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Ex12Test {
 
     @Test
-    public void test_tinh() {
+    public void test_canbac2() {
         assertEquals(String.format("%.3f", new Ex12().canbac2(4)), "2.000");
         assertEquals(String.format("%.3f", new Ex12().canbac2(1)), "1.000");
         assertEquals(String.format("%.3f", new Ex12().canbac2(5)), "2.236");
