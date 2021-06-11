6a9bbb00216a85802802f60e5cd127eea8c9d6f6
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-11 10:43:41 +0700

    add-exercise-17

--- src/main/java/Ex17.java
+++ src/main/java/Ex17.java
@@ -9,7 +9,7 @@ public class Ex17 {
         if (n == 0) return 0;
         if (n == 1) return 1;
         double xL = 0;
-        double xR = n < 1 ? 1 : n;
+        double xR = n;
         while (xR - xL > epsilon) {
             double xM = (xL + xR) / 2;
             if (func(xM, n) < 0)
