733c352a8e08abea004477bc89f1ed9ec4707901
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-11 11:04:28 +0700

    add-exercise-18

---
+++ src/main/java/Ex18.java
@@ -0,0 +1,40 @@
+public class Ex18 {
+//    I   V   X   L   C   D   M
+//    1   5   10  50  100 500 1000
+//    Chữ số đi sau chữ số khác lớn hơn hoặc bằng thì cộng thêm vào:
+//    VI = V + I = 5 + 1 = 6
+//    LX = L + X = 50 + 10 = 60
+//    XX = X + X = 10 + 10 = 20
+//
+//    Chữ số đi trước chữ số khác lớn hơn thì trừ bớt đi:
+//    IV = V – I = 5 – 1
+//    XL = L – X = 50 – 10 = 40
+    public int covertToDecimal(String roman) {
+        int[] a = new int[roman.length()];
+        for (int i = 0; i < roman.length(); i++) {
+            if (roman.charAt(i) == 'I')
+                a[i] = 1;
+            else if (roman.charAt(i) == 'V')
+                a[i] = 5;
+            else if (roman.charAt(i) == 'X')
+                a[i] = 10;
+            else if (roman.charAt(i) == 'L')
+                a[i] = 50;
+            else if (roman.charAt(i) == 'C')
+                a[i] = 100;
+            else if (roman.charAt(i) == 'D')
+                a[i] = 500;
+            else if (roman.charAt(i) == 'M')
+                a[i] = 1000;
+        }
+        int k = 0;
+        k = a[roman.length() - 1];
+        for (int i = roman.length() - 1; i > 0; i--) {
+            if (a[i] > a[i - 1])
+                k = k - a[i - 1];
+            else if (a[i] == a[i - 1] || a[i] < a[i - 1])
+                k = k + a[i - 1];
+        }
+        return k;
+    }
+}
---
+++ src/test/java/Ex18Test.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Ex18Test {
+
+    @Test
+    public void test() {
+        assertEquals(new Ex18().covertToDecimal("VIM"), 1004);
+        assertEquals(new Ex18().covertToDecimal("XXXII"), 32);
+        assertEquals(new Ex18().covertToDecimal("XLV"), 45);
+        assertEquals(new Ex18().covertToDecimal("MMMDCCCLXXXVIII"), 3888);
+        assertEquals(new Ex18().covertToDecimal("MMMCMXCIX "), 3999);
+        assertEquals(new Ex18().covertToDecimal("I"), 1);
+    }
+}
