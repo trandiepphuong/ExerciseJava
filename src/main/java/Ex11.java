92a3af80d25ff19b234ae4673d57b5e622c84c43
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-11 09:11:17 +0700

    update-exercise-11

--- src/main/java/Ex11.java
+++ src/main/java/Ex11.java
@@ -1,44 +1,8 @@
 public class Ex11 {
     public String thang(int n) {
-        String thang = "";
-        switch (n) {
-            case (1):
-                thang = "thang mot";
-                break;
-            case (2):
-                thang = "thang hai";
-                break;
-            case (3):
-                thang = "thang ba";
-                break;
-            case (4):
-                thang = "thang tu";
-                break;
-            case (5):
-                thang = "thang nam";
-                break;
-            case (6):
-                thang = "thang sau";
-                break;
-            case (7):
-                thang = "thang bay";
-                break;
-            case (8):
-                thang = "thang tam";
-                break;
-            case (9):
-                thang = "thang chin";
-                break;
-            case (10):
-                thang = "thang muoi";
-                break;
-            case (11):
-                thang = "thang muoi mot";
-                break;
-            case (12):
-                thang = "thang muoi hai";
-                break;
-        }
-        return thang;
+        String array[] = {"thang 1", "thang 2", "thang 3", "thang 4",
+                          "thang 5", "thang 6", "thang 7", "thang 8",
+                          "thang 9", "thang 10", "thang 11", "thang 12"};
+        return array[n-1];
     }
 }
--- src/test/java/Ex11Test.java
+++ src/test/java/Ex11Test.java
@@ -6,17 +6,17 @@ public class Ex11Test {
 
     @Test
     public void test_thang() {
-        assertEquals(new Ex11().thang(1), "thang mot");
-        assertEquals(new Ex11().thang(2), "thang hai");
-        assertEquals(new Ex11().thang(3), "thang ba");
-        assertEquals(new Ex11().thang(4), "thang tu");
-        assertEquals(new Ex11().thang(5), "thang nam");
-        assertEquals(new Ex11().thang(6), "thang sau");
-        assertEquals(new Ex11().thang(7), "thang bay");
-        assertEquals(new Ex11().thang(8), "thang tam");
-        assertEquals(new Ex11().thang(9), "thang chin");
-        assertEquals(new Ex11().thang(10), "thang muoi");
-        assertEquals(new Ex11().thang(11), "thang muoi mot");
-        assertEquals(new Ex11().thang(12), "thang muoi hai");
+        assertEquals(new Ex11().thang(1), "thang 1");
+        assertEquals(new Ex11().thang(2), "thang 2");
+        assertEquals(new Ex11().thang(3), "thang 3");
+        assertEquals(new Ex11().thang(4), "thang 4");
+        assertEquals(new Ex11().thang(5), "thang 5");
+        assertEquals(new Ex11().thang(6), "thang 6");
+        assertEquals(new Ex11().thang(7), "thang 7");
+        assertEquals(new Ex11().thang(8), "thang 8");
+        assertEquals(new Ex11().thang(9), "thang 9");
+        assertEquals(new Ex11().thang(10), "thang 10");
+        assertEquals(new Ex11().thang(11), "thang 11");
+        assertEquals(new Ex11().thang(12), "thang 12");
     }
 }
