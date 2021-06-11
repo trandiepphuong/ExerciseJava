37930c2dacce6a8313d51f7866b8c65e6054ffe2
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-11 09:15:22 +0700

    update-exercise-10

--- src/main/java/Ex10.java
+++ src/main/java/Ex10.java
@@ -1,36 +1,14 @@
 public class Ex10 {
     public String thu(int ngay, int thang, int nam) {
-        String result = "";
         //nam =1900 + (nam%1900);
         if (thang < 3) {
             thang = thang + 12;
             nam = nam - 1;
         }
         int thu = (ngay + 2 * thang + (3 * (thang + 1)) / 5 + nam + (nam / 4)) % 7;
-        switch (thu) {
-            case (0):
-                result = "chu nhat";
-                break;
-            case (1):
-                result = "thu hai";
-                break;
-            case (2):
-                result = "thu ba";
-                break;
-            case (3):
-                result = "thu tu";
-                break;
-            case (4):
-                result = "thu nam";
-                break;
-            case (5):
-                result = "thu sau";
-                break;
-            case (6):
-                result = "thu bay";
-                break;
-        }
-        return result;
+        String array[] = {"chu nhat", "thu 2", "thu 3", "thu 4",
+                "thu 5", "thu 6", "thu 7"};
+        return array[thu];
     }
 }
 
--- src/test/java/Ex10Test.java
+++ src/test/java/Ex10Test.java
@@ -6,12 +6,12 @@ public class Ex10Test {
 
     @Test
     public void test_thu() {
-        assertEquals(new Ex10().thu(1, 2, 2021), "thu hai");
+        assertEquals(new Ex10().thu(1, 2, 2021), "thu 2");
         assertEquals(new Ex10().thu(6, 6, 2021), "chu nhat");
-        assertEquals(new Ex10().thu(8, 6, 2021), "thu ba");
-        assertEquals(new Ex10().thu(9, 6, 2021), "thu tu");
-        assertEquals(new Ex10().thu(10, 6, 2021), "thu nam");
-        assertEquals(new Ex10().thu(11, 6, 2021), "thu sau");
-        assertEquals(new Ex10().thu(12, 6, 2021), "thu bay");
+        assertEquals(new Ex10().thu(8, 6, 2021), "thu 3");
+        assertEquals(new Ex10().thu(9, 6, 2021), "thu 4");
+        assertEquals(new Ex10().thu(10, 6, 2021), "thu 5");
+        assertEquals(new Ex10().thu(11, 6, 2021), "thu 6");
+        assertEquals(new Ex10().thu(12, 6, 2021), "thu 7");
     }
 }
