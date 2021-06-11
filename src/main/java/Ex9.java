b2fa82aaef0358135cead62d499fd735793bc091
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-11 15:40:32 +0700

    add-exercise-09

---
+++ src/main/java/Ex9.java
@@ -0,0 +1,19 @@
+public class Ex9 {
+    public static final double E = 0.00001;
+
+    public double pi() {
+        double sum = 0;
+        double e = 1;
+        int n = 1;
+        while (e > E) {
+            e = (double) 4 / ((2 * n) * (2 * n + 1) * (2 * n + 2));
+            if (n % 2 != 0)
+                sum += e;
+            else
+                sum -= e;
+            n++;
+        }
+        sum += 3;
+        return sum;
+    }
+}
