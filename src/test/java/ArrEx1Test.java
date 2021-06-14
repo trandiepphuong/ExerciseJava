66195843d2688f902bf6823bf43c6a01fb070a4b
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-14 09:30:44 +0700

    add-arr-exercise-01

---
+++ src/test/java/ArrEx1Test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrEx1Test {
+
+    @Test
+    public void test_twiceInArr(){
+        assertEquals(new ArrEx1().twiceInArr(new int[]{1, 2, 3, 4, 5}), "");
+        assertEquals(new ArrEx1().twiceInArr(new int[]{1, 1, 3, 4, 5}), "1");
+        assertEquals(new ArrEx1().twiceInArr(new int[]{1, 1, 2, 2, 3}), "1, 2");
+    }
+}
