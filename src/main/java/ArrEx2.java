a5edc49bb19198a62c3cfbcc8229756bfd6868c9
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-14 09:46:12 +0700

    add-arr-exercise-02

--- src/main/java/ArrEx2.java
+++ src/main/java/ArrEx2.java
@@ -4,9 +4,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class ArrEx2 {
-    public int sum3Max(int[] a){
-        int sum = 0 ;
+    public int sum3Max(int[] a) {
+        int sum = 0;
         Arrays.sort(a);
-        return a[a.length-1]+a[a.length-2]+a[a.length-3];
+        return a[a.length - 1] + a[a.length - 2] + a[a.length - 3];
     }
 }
