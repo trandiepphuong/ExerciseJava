0fd9d5f0e4a5647154746dd63eb3dc91d594bb05
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-15 14:38:34 +0700

    add-err-exercise-07

--- src/main/java/ArrEx7.java
+++ src/main/java/ArrEx7.java
@@ -1,11 +1,11 @@
 public class ArrEx7 {
-    public int sub(int[] arr){
+    public int sub(int[] arr) {
         int max = arr[0];
         int min = arr[0];
-        for (int i = 0 ; i<arr.length;i++){
-            if(arr[i]>max) max = arr[i];
-            if(arr[i]<min) min = arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
         }
-        return max-min;
+        return max - min;
     }
 }
--- src/test/java/ArrEx7Test.java
+++ src/test/java/ArrEx7Test.java
@@ -5,8 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ArrEx7Test {
 
     @Test
-    public void test_sub(){
-        assertEquals(new ArrEx7().sub(new int[] {1,2,3,4,5}), 4);
-        assertEquals(new ArrEx7().sub(new int[] {1,2,3,4,5,-6}), 11);
+    public void test_sub() {
+        assertEquals(new ArrEx7().sub(new int[]{1, 2, 3, 4, 5}), 4);
+        assertEquals(new ArrEx7().sub(new int[]{1, 2, 3, 4, 5, -6}), 11);
     }
 }
