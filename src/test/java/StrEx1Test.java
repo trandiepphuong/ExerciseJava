48bfa4754b5e7bfa0f963aca35af8dd7122e80a0
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-16 14:59:29 +0700

    add-str-exercise-01

--- src/test/java/StrEx1Test.java
+++ src/test/java/StrEx1Test.java
@@ -5,6 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StrEx1Test {
     @Test
     public void test_split() {
-        assertEquals(new StrEx1().split("a.b!c?d\\ne"), "a, b, c, d, e");
+        assertEquals(new StrEx1().split("a.b!c?d\ne"), "a, b, c, d, e");
     }
 }
