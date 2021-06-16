619f323732efebd5d23bc5c8992f74b2938c36ab
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-16 14:15:35 +0700

    update-exercise-12

--- src/test/java/Ex12Test.java
+++ src/test/java/Ex12Test.java
@@ -6,8 +6,8 @@ public class Ex12Test {
 
     @Test
     public void test_canbac2() {
-        assertEquals(new Ex12().canbac2(4), 2, new Ex12().PRECISE);
-        assertEquals(new Ex12().canbac2(1), 1, new Ex12().PRECISE);
-        assertEquals(new Ex12().canbac2(5), 2.236, new Ex12().PRECISE);
+        assertEquals(new Ex12().canbac2(4), 2, Ex12.PRECISE);
+        assertEquals(new Ex12().canbac2(1), 1, Ex12.PRECISE);
+        assertEquals(new Ex12().canbac2(5), 2.236, Ex12.PRECISE);
     }
 }
