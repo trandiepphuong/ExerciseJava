31b108c59a5262294dcc0df1374921d67ce2d818
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-16 16:16:05 +0700

    update-arr-exercise-13

--- src/main/java/ArrEx13.java
+++ src/main/java/ArrEx13.java
@@ -6,17 +6,19 @@ public class ArrEx13 {
         int cols = arr[0].length;
         int[][] newArr = new int[rows - 1][cols - 1];
         int p = 0;
-        for (int i = 0; i < rows; ++i) {
-            if (i == delRow)
+        for (int i = 0; i < rows; i++) {
+            if (i == delRow) {
                 continue;
-            int q = 0;
-            for (int j = 0; j < cols; ++j) {
-                if (j == delCol)
-                    continue;
-                newArr[p][q] = arr[i][j];
-                ++q;
             }
-            ++p;
+            int q = 0;
+            for (int j = 0; j < cols; j++) {
+                if (j == delCol) {
+                    continue;
+                }
+                newArr[p][q] = arr[i][j];
+                q++;
+            }
+            p++;
         }
         return Arrays.deepToString(newArr);
     }
