d4ed172114bb279bb5db873660dbab44f2b743a1
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-16 13:50:02 +0700

    update-exercise-19

--- src/main/java/Ex19.java
+++ src/main/java/Ex19.java
@@ -1,11 +1,11 @@
 public class Ex19 {
+    static final String[] romans = new String[]{"M", "CM", "D", "CD", "C", "XC", "L",
+            "XL", "X", "IX", "V", "IV", "I"};
+    static final int[] ints = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5,
+            4, 1};
+
     public String covertToRoman(int decimal) {
         String result = new String();
-        int times = 0;
-        String[] romans = new String[]{"M", "CM", "D", "CD", "C", "XC", "L",
-                "XL", "X", "IX", "V", "IV", "I"};
-        int[] ints = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5,
-                4, 1};
         for (int i = 0; i < ints.length; i++) {
             int currentNum = decimal / ints[i];
             if (currentNum == 0) {
