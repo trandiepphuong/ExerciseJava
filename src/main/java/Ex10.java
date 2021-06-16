83d718f8f33a5a63b62f6ac7a6e0983f67f7f193
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-16 15:52:25 +0700

    update-exercise-10

--- src/main/java/Ex10.java
+++ src/main/java/Ex10.java
@@ -1,13 +1,12 @@
 public class Ex10 {
+    public static String array[] = {"chu nhat", "thu 2", "thu 3", "thu 4",
+            "thu 5", "thu 6", "thu 7"};
     public String thu(int ngay, int thang, int nam) {
-        //nam =1900 + (nam%1900);
         if (thang < 3) {
             thang = thang + 12;
             nam = nam - 1;
         }
         int thu = (ngay + 2 * thang + (3 * (thang + 1)) / 5 + nam + (nam / 4)) % 7;
-        String array[] = {"chu nhat", "thu 2", "thu 3", "thu 4",
-                "thu 5", "thu 6", "thu 7"};
         return array[thu];
     }
 }
