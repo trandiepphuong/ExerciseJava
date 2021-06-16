9c95bf926a352fd111748cf8fb8540a9630e7a7b
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-16 16:05:16 +0700

    update-exercise-03

--- src/main/java/Ex3.java
+++ src/main/java/Ex3.java
@@ -3,16 +3,16 @@ import java.util.List;
 
 public class Ex3 {
     public String phantichNT(int n) {
-        String result = "";
+        List<String> result = new ArrayList<>();
         int i = 2;
         while (n > 1) {
             if (n % i == 0) {
                 n = n / i;
-                result += i;
+                result.add(String.valueOf(i));
             } else {
                 i++;
             }
         }
-        return result;
+        return String.join("*",result);
     }
 }
--- src/test/java/Ex3Test.java
+++ src/test/java/Ex3Test.java
@@ -7,7 +7,7 @@ public class Ex3Test {
     @Test
     public void test_phantichNT(){
         assertEquals(new Ex3().phantichNT(1), "");
-        assertEquals(new Ex3().phantichNT(600), "222355");
-        assertEquals(new Ex3().phantichNT(6), "23");
+        assertEquals(new Ex3().phantichNT(600), "2*2*2*3*5*5");
+        assertEquals(new Ex3().phantichNT(6), "2*3");
     }
 }
