3d61c437564d29fbb3cbe0e9cf9a22618ec6974a
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-16 15:58:01 +0700

    update-exercise-18

--- src/main/java/Ex18.java
+++ src/main/java/Ex18.java
@@ -1,5 +1,5 @@
 public class Ex18 {
-//    I   V   X   L   C   D   M
+    //    I   V   X   L   C   D   M
 //    1   5   10  50  100 500 1000
 //    Chữ số đi sau chữ số khác lớn hơn hoặc bằng thì cộng thêm vào:
 //    VI = V + I = 5 + 1 = 6
@@ -12,23 +12,39 @@ public class Ex18 {
     public int covertToDecimal(String roman) {
         int[] a = new int[roman.length()];
         for (int i = 0; i < roman.length(); i++) {
-            if (roman.charAt(i) == 'I')
-                a[i] = 1;
-            else if (roman.charAt(i) == 'V')
-                a[i] = 5;
-            else if (roman.charAt(i) == 'X')
-                a[i] = 10;
-            else if (roman.charAt(i) == 'L')
-                a[i] = 50;
-            else if (roman.charAt(i) == 'C')
-                a[i] = 100;
-            else if (roman.charAt(i) == 'D')
-                a[i] = 500;
-            else if (roman.charAt(i) == 'M')
-                a[i] = 1000;
+            char temp = roman.charAt(i);
+            switch (temp) {
+                case ('I'): {
+                    a[i] = 1;
+                    break;
+                }
+                case ('V'): {
+                    a[i] = 5;
+                    break;
+                }
+                case ('X'): {
+                    a[i] = 10;
+                    break;
+                }
+                case ('L'): {
+                    a[i] = 50;
+                    break;
+                }
+                case ('C'): {
+                    a[i] = 100;
+                    break;
+                }
+                case ('D'): {
+                    a[i] = 500;
+                    break;
+                }
+                case ('M'): {
+                    a[i] = 1000;
+                    break;
+                }
+            }
         }
-        int k = 0;
-        k = a[roman.length() - 1];
+        int k = a[roman.length() - 1];
         for (int i = roman.length() - 1; i > 0; i--) {
             if (a[i] > a[i - 1])
                 k = k - a[i - 1];
