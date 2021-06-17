bea1ae586dd7a037d10bc88434a2da35c7b73e27
Vo Quang Hoa <voquanghoa@gmail.com>
2021-06-17 16:45:34 +0700

    Add failed test case

--- src/test/java/StrEx7Test.java
+++ src/test/java/StrEx7Test.java
@@ -6,6 +6,8 @@ public class StrEx7Test {
     @Test
     public void test_dich() {
         assertEquals(new StrEx7().dich("ab13c"), "abbbbbbbbbbbbbc");
+        assertEquals(new StrEx7().dich("a12b23c3"), "a".repeat(12) + "b".repeat(23) + "c".repeat(3));
+        assertEquals(new StrEx7().dich("a234b23c33"), "a".repeat(234) + "b".repeat(23) + "c".repeat(33));
         assertEquals(new StrEx7().dich("abc4e6fd"), "abcccceeeeeefd");
     }
 }
