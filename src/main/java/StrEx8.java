886a06441b217400b230cf10489c36dded31924d
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-17 11:07:10 +0700

    add-str-exercise-08

---
+++ src/main/java/StrEx8.java
@@ -0,0 +1,14 @@
+public class StrEx8 {
+    public boolean checkBarcode(String n) {
+        int sum = 0;
+        for (int i = 0; i < n.length(); i++) {
+            int digit = Integer.parseInt(String.valueOf(n.charAt(i)));
+            if ((i + 1) % 2 != 0) {
+                sum += digit;
+            } else {
+                sum += 3 * digit;
+            }
+        }
+        return sum % 10 == 0;
+    }
+}
