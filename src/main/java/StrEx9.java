95ff30562c1574ba605b3aa420e0c2b931be5d99
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-17 11:27:19 +0700

    add-str-exercise-09

---
+++ src/main/java/StrEx9.java
@@ -0,0 +1,16 @@
+public class StrEx9 {
+    public int verifyBarcode(String n) {
+        int sum = 0;
+        for (int i = 0; i < n.length(); i++) {
+            int digit = Integer.parseInt(String.valueOf(n.charAt(i)));
+            if ((i + 1) % 2 != 0) {
+                sum += digit;
+            } else {
+                sum += 3 * digit;
+            }
+        }
+        System.out.println(sum);
+        int rounded = ((sum + 9) / 10 ) * 10;
+        return rounded-sum;
+    }
+}
