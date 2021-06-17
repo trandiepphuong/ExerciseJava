a73662fc8bfe4db0759e503a11730413df738e01
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-17 08:52:55 +0700

    update-str-exercise-02

--- src/main/java/StrEx2.java
+++ src/main/java/StrEx2.java
@@ -1,10 +1,9 @@
 public class StrEx2 {
     public boolean checkPalindrome(String n) {
-        int length = n.length();
         String reverseStr = "";
-        for (int i = length - 1; i >= 0; i--)
+        for (int i = n.length() - 1; i >= 0; i--) {
             reverseStr = reverseStr + n.charAt(i);
-        if (n.equals(reverseStr)) return n.equals(reverseStr);
-        return false;
+        }
+        return n.equals(reverseStr);
     }
 }
