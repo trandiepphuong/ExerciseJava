d0ba6dbcffba5b938cb0d950e6224820208276c2
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-21 11:20:51 +0700

    update-arr-exercise-06

--- src/main/java/ArrEx6.java
+++ src/main/java/ArrEx6.java
@@ -3,24 +3,24 @@ import java.util.List;
 
 public class ArrEx6 {
     public String findMaxLenght(int[] arr) {
-        List<String> result = new ArrayList<>();
         int n = arr.length;
-        int length = 1;
+        int maxLength = 1;
         int max = 1;
         int start = 0;
         int end = 0;
         for (int i = 0; i < n - 1; i++) {
             if (arr[i + 1] - arr[i] == 1) {
-                length++;
-                if (length > max) {
-                    max = length;
-                    start = i + 2 - length;
+                maxLength++;
+                if (maxLength > max) {
+                    max = maxLength;
+                    start = i + 2 - maxLength;
                     end = i + 2;
                 }
             } else {
-                length = 1;
+                maxLength = 1;
             }
         }
+        List<String> result = new ArrayList<>();
         for (int i = start; i < end; i++)
             result.add(String.valueOf(arr[i]));
         return String.join(", ", result);
