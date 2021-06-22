d1e530026106bff6973e6571048456f97bb531fa
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-22 10:18:38 +0700

    update-oop-exercise-01

--- src/main/java/StudentController.java
+++ src/main/java/StudentController.java
@@ -1,4 +1,5 @@
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
@@ -7,52 +8,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StudentController {
-    //data
-    public List<Student> dataStudent = new ArrayList<Student>();
-    public void addStudent(Student student) {
+    public static final Gson gson = new Gson();
+
+    public void addStudent(List<Student> dataStudent, Student student) {
         dataStudent.add(student);
     }
-    public void deleteStudent(Student student){
+
+    public void deleteStudent(List<Student> dataStudent, Student student) {
         dataStudent.remove(student);
     }
-    public void saveToFile(String path) throws IOException {
-        Gson gson = new Gson();
-        FileWriter f = new FileWriter(new File(path,"students.json"));
+
+    public void saveToFile(String path, List<Student> dataStudent) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        FileWriter f = new FileWriter(new File(path));
         f.write(gson.toJson(dataStudent));
         f.flush();
     }
-    public void loadFromFile(String path) throws IOException {
-        Gson gson = new Gson();
+
+    public static List<Student> loadFromFile(String path) throws IOException {
         StringBuilder jsonStr = new StringBuilder();
-        FileReader f = new FileReader(new File("C:\\Users\\DELL\\Desktop", "students.json"));
+        FileReader f = new FileReader(new File(path));
         BufferedReader reader = new BufferedReader(f);
         String line = reader.readLine();
         while (line != null) {
             jsonStr.append(line);
             line = reader.readLine();
         }
-        Type studentType = new TypeToken<Student>(){}.getType();
-        List<Student> dataStudent = gson.fromJson(jsonStr, studentType);
+
+        Type listOfMyClassObject = new TypeToken<ArrayList<Student>>() {
+        }.getType();
+        List<Student> outputList = gson.fromJson(jsonStr.toString(), listOfMyClassObject);
+
+        return outputList;
     }
-    public Student getById(int id){
-        for (Student student : dataStudent){
-            if(student.getId()==id){
+
+    public Student getById(List<Student> dataStudent, int id) {
+        for (Student student : dataStudent) {
+            if (student.getId() == id) {
                 return student;
             }
         }
         return null;
     }
-    public Student findByName(String name){
-        for (Student student : dataStudent){
-            if(student.getFirstName().equals(name)){
+
+    public Student findByName(List<Student> dataStudent, String name) {
+        for (Student student : dataStudent) {
+            if (student.getFirstName().equals(name)) {
                 return student;
             }
         }
         return null;
     }
-    public Student findByClassName(String className){
-        for (Student student : dataStudent){
-            if(student.getClassName().equals(className)){
+
+    public Student findByClassName(List<Student> dataStudent, String className) {
+        for (Student student : dataStudent) {
+            if (student.getClassName().equals(className)) {
                 return student;
             }
         }
