8b26fc92b259b6b559d94e2c7025279f254c0df4
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-23 09:58:41 +0700

    update-arr-exercise-10

--- src/main/java/ArrEx10.java
+++ src/main/java/ArrEx10.java
@@ -1,24 +1,20 @@
 import java.util.Arrays;
 
 public class ArrEx10 {
-    public int[] addElement(int[] a, int pos, int num) {
-        int[] result = new int[a.length + 1];
-        for (int i = 0; i < pos; i++) {
-            result[i] = a[i];
-        }
-        result[pos] = num;
-        for (int i = pos; i < a.length; i++) {
-            result[i + 1] = a[i];
-        }
-        return result;
-    }
-
     public String insert(int arr[], int x) {
         int[] newArr = new int[arr.length];
         int i;
         for (i = 0; i < arr.length; i++) {
             if (x < arr[i]) {
-                return Arrays.toString(addElement(arr, i, x));
+                int[] result = new int[arr.length + 1];
+                for (int j = 0; j < i; j++) {
+                    result[j] = arr[j];
+                }
+                result[i] = x;
+                for (int j = i; j < arr.length; j++) {
+                    result[j + 1] = arr[j];
+                }
+                return Arrays.toString(result);
             } else {
                 newArr = Arrays.copyOf(arr, arr.length + 1);
                 newArr[arr.length] = x;
