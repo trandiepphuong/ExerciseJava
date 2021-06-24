48f76cf53a29747fafdd013e193f6130eb59c29d
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-24 14:36:05 +0700

    update-oop-exercise-02

--- src/test/java/OopExerciseTest/stackTest.java
+++ src/test/java/OopExerciseTest/stackTest.java
@@ -9,11 +9,20 @@ public class stackTest {
         s.push(1);
         s.push(2);
         s.push(3);
-        assertEquals(s.toString(), "[1, 2, 3]");
-        assertEquals(s.peek(), 3);
-        assertEquals(s.size(), 3);
-        assertEquals(s.search(3), 1);
-        assertEquals(s.pop(), 3);
-        assertEquals(s.toString(), "[1, 2]");
+        s.push(4);
+        s.push(5);
+        s.push(6);
+        s.push(7);
+        s.push(8);
+        s.push(9);
+        s.push(10);
+        assertEquals(s.toString(), "[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
+        assertEquals(s.peek(), 10);
+        assertEquals(s.size(), 10);
+        assertEquals(s.search(10), 1);
+        assertEquals(s.search(1), 10);
+        assertEquals(s.search(0), -1);
+        assertEquals(s.pop(), 10);
+        assertEquals(s.toString(), "[1, 2, 3, 4, 5, 6, 7, 8, 9]");
     }
 }
