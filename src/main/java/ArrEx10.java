e33bd7bf5126764f0ac55b494aee85deea27db2e
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-25 16:51:23 +0700

    update-arr-exercise-10

--- src/main/java/ArrEx10.java
+++ src/main/java/ArrEx10.java
@@ -2,25 +2,25 @@ import java.util.Arrays;
 
 public class ArrEx10 {
     public String insert(int arr[], int x) {
-        int[] newArr = new int[arr.length];
-        int i;
-        for (i = 0; i < arr.length; i++) {
-            if (x < arr[i]) {
-                int[] result = new int[arr.length + 1];
+        boolean isMid = false;
+        int[] newArr = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            if (x < arr[i] && x > arr[i - 1]) {
                 for (int j = 0; j < i; j++) {
-                    result[j] = arr[j];
+                    newArr[j] = arr[j];
                 }
-                result[i] = x;
-                for (int j = i; j < arr.length; j++) {
-                    result[j + 1] = arr[j];
+                newArr[i] = x;
+                for (int j = i + 1; j < newArr.length; j++) {
+                    newArr[j] = arr[j - 1];
                 }
-                return Arrays.toString(result);
-            } else {
-                newArr = Arrays.copyOf(arr, arr.length + 1);
-                newArr[arr.length] = x;
+                isMid = true;
+                break;
             }
+        }
+        if (isMid==false){
+            newArr = Arrays.copyOf(arr, arr.length + 1);
+            newArr[arr.length] = x;
         }
         return Arrays.toString(newArr);
     }
-
 }
