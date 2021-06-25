e2f355973e2fec2a3f3aeea59f6f0ae0ca184fac
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-25 14:58:55 +0700

    update-str-exercise-09

--- src/main/java/StrEx9.java
+++ src/main/java/StrEx9.java
@@ -1,18 +1,14 @@
 public class StrEx9 {
-    int round(int n) {
-        return ((n + 9) / 10) * 10;
-    }
-
     public int verifyBarcode(String n) {
         int sum = 0;
         for (int i = 0; i < n.length(); i++) {
-            int digit = Integer.parseInt(String.valueOf(n.charAt(i)));
-            if ((i + 1) % 2 != 0) {
+            int digit = n.charAt(i) - '0';
+            if (i % 2 == 0) {
                 sum += digit;
             } else {
                 sum += 3 * digit;
             }
         }
-        return round(sum) - sum;
+        return (10 - sum % 10) % 10;
     }
 }
