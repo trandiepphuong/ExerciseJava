ae9329255eb8cadc46967d031d3fe9ef16302619
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-25 15:29:55 +0700

    update-str-exercise-08

--- src/main/java/StrEx8.java
+++ src/main/java/StrEx8.java
@@ -3,7 +3,7 @@ public class StrEx8 {
         int sum = 0;
         for (int i = 0; i < n.length(); i++) {
             int digit = n.charAt(i) - '0';
-            if ((i + 1) % 2 != 0) {
+            if (i % 2 == 0) {
                 sum += digit;
             } else {
                 sum += 3 * digit;
