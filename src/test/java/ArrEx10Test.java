cef3d16b79886c63ed6cff066f0f7591d56c8fc2
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-06-28 13:35:02 +0700

    update-arr-exercise-10

--- src/test/java/ArrEx10Test.java
+++ src/test/java/ArrEx10Test.java
@@ -6,7 +6,8 @@ public class ArrEx10Test {
 
     @Test
     public void test_insert() {
-        assertEquals(new ArrEx10().insert(new int[]{1, 2, 4, 5},3), "[1, 2, 3, 4, 5]");
-        assertEquals(new ArrEx10().insert(new int[]{1, 2, 4, 5},6), "[1, 2, 4, 5, 6]");
+        assertEquals(new ArrEx10().insert(new int[]{1, 2, 4, 5}, 3), "[1, 2, 3, 4, 5]");
+        assertEquals(new ArrEx10().insert(new int[]{1, 2, 4, 5}, 0), "[0, 1, 2, 4, 5]");
+        assertEquals(new ArrEx10().insert(new int[]{1, 2, 4, 5}, 6), "[1, 2, 4, 5, 6]");
     }
 }
